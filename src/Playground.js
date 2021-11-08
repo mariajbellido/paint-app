c9843c669b09f2a0c097944671aea64bb33af973
Maria Jesus Bellido <mariajbellido@gmail.com>
2021-11-08 13:51:12 +0100

    useState - part 1

--- src/Playground.js
+++ src/Playground.js
@@ -1,12 +1,19 @@
-import React, {
-  useState,
-  useEffect,
-  useRef,
-  useCallback,
-  useMemo,
-} from "react";
+import React, { useState } from "react";
 import randomColor from "randomcolor";
 
 export default function Playground() {
-  return <div className="playground">Hello Worlds!</div>;
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      {count}
+      <button onClick={() => setCount((currentCount) => currentCount - 1)}>
+        -
+      </button>
+      <button onClick={() => setCount((currentCount) => currentCount + 1)}>
+        +
+      </button>
+    </div>
+  );
 }
+
+// Podríamos usar count directamente, pero al manejarse el estado de forma asíncrona podríamos llegar a ver valores que no fueran correctos.
