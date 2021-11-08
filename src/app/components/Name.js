f62fc7245eeaf36b50d8cca68eb9e6a6cb2cfc28
Maria Jesus Bellido <mariajbellido@gmail.com>
2021-11-08 14:43:29 +0100

    useState - part 2

---
+++ src/app/components/Name.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import "../../style.css";
+
+export default function Name() {
+  const [name, setName] = useState("");
+
+  return (
+    <label className="header-name">
+      <input
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+        onClick={(event) =>
+          event.target.setSelectionRange(0, event.target.value.length)
+        }
+        placeholder="Untitled"
+      />
+    </label>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Playground from "./Playground";
-
-ReactDOM.render(<Playground />, document.getElementById("root"));
+// import Playground from "./Playground";
+import Paint from "./app/components/Paint";
+ReactDOM.render(<Paint />, document.getElementById("root"));
