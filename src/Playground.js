2f9ae2b2314050f231de6df60b7d3ac64d5d6d24
Maria Jesus Bellido <mariajbellido@gmail.com>
2021-11-08 16:46:52 +0100

    useMemo + useCallback quick overview and examples (playground)

--- src/Playground.js
+++ src/Playground.js
@@ -1,14 +1,22 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  useCallback,
+  useMemo,
+} from "react";
 import randomColor from "randomcolor";
 
 export default function Playground() {
-  const [count, setCount] = useState(0);
-  const [color, setColor] = useState(null);
+  const [count, setCount] = useState(30);
+  const [color, setColor] = useState(randomColor());
+
+  const cb = useCallback((num) => console.log(num), [count]);
 
   const inputRef = useRef();
 
   useEffect(() => {
-    setColor(randomColor());
+    //setColor(randomColor());
     inputRef.current.focus();
   }, [count]);
 
@@ -16,6 +24,10 @@ export default function Playground() {
   // [] -> el render se produce una sola vez
   // [count] -> se renderiza de nuevo cada vez que cambia count
   // sin dependencias -> bucle infinito
+
+  useCallback(() => console.log("useCallback")); // retornamos la función
+  useMemo(() => console.log("useMemo")); // retorna el RESULTADO de la función
+  // useMemo(() => () => console.log("useMemo"))  ambos hooks, useCallBack y useMemo serían ahora equivalentes
 
   return (
     <div style={{ borderTop: `10px solid ${color}` }}>
@@ -26,6 +38,7 @@ export default function Playground() {
       <button onClick={() => setCount((currentCount) => currentCount + 1)}>
         +
       </button>
+      <button onClick={() => setColor(randomColor())}>Change Color</button>
       <hr />
       <input
         ref={inputRef}
@@ -33,6 +46,7 @@ export default function Playground() {
         onChange={(event) => setCount(event.target.value)}
         value={count}
       />
+      <Calculate cb={cb} num={count} />
     </div>
   );
 }
@@ -41,3 +55,9 @@ export default function Playground() {
 
 // Performande inline functions
 // https://cdb.reacttraining.com/react-inline-functions-and-performance-bdff784f5578
+
+const Calculate = React.memo(({ cb, num }) => {
+  cb(num);
+  const renderCount = useRef(1); // useRef mantenderá el valor entre renderizados
+  return <div>{renderCount.current++}</div>;
+});
