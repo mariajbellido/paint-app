f5feda1adc34e513bd6c42f2f19281b7a8022c84
Maria Jesus Bellido <mariajbellido@gmail.com>
2021-11-08 14:55:14 +0100

    useEffect

--- src/Playground.js
+++ src/Playground.js
@@ -1,9 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import randomColor from "randomcolor";
 
 export default function Playground() {
   const [count, setCount] = useState(0);
+  const [color, setColor] = useState(null);
+
+  useEffect(() => {
+    setColor(randomColor());
+  }, [count]);
+
+  // USE EFFECT
+  // [] -> el render se produce una sola vez
+  // [count] -> se renderiza de nuevo cada vez que cambia count
+  // sin dependencias -> bucle infinito
+
   return (
-    <div>
+    <div style={{ borderTop: `10px solid ${color}` }}>
       {count}
       <button onClick={() => setCount((currentCount) => currentCount - 1)}>
         -
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// import Playground from "./Playground";
-import Paint from "./app/components/Paint";
-ReactDOM.render(<Paint />, document.getElementById("root"));
+//import Paint from "./app/components/Paint";
+import Playground from "./Playground";
+
+ReactDOM.render(
+  <Playground />,
+
+  //<Paint />,
+  document.getElementById("root")
+);
